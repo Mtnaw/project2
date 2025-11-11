d1410f2d7a8ec4e8b0ae91f770ddd1e9eb50864d
nanntthein <tthi86681@gmail.com>
2025-11-11 14:30:53 -0800

    change home to house

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,9 +4,8 @@ import { SessionProvider } from 'next-auth/react'
 
 interface ProvidersProps {
   children: React.ReactNode
-  session: any
 }
 
-export default function Providers({ children, session }: ProvidersProps) {
-  return <SessionProvider session={session}>{children}</SessionProvider>
+export default function Providers({ children }: ProvidersProps) {
+  return <SessionProvider>{children}</SessionProvider>
 }
